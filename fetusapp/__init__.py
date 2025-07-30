be634abeee26cd32fb3633fc01b712d11738d04e
zdetor54 <zdetor54@gmail.com>
2025-07-30 09:52:16 +0100

    fix circular import

--- fetusapp/__init__.py
+++ fetusapp/__init__.py
@@ -5,9 +5,6 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
-from . import models
-
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -51,6 +48,8 @@ csrf.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
+
+from . import models
 
 
 from fetusapp.core.views import core
